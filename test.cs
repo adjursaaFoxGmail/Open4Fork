73d97e85f3d6a490c83bdb33b2b3fbf134140ce8
Anders Djursaa <59800490+adjuc@users.noreply.github.com>
2021-04-06 16:52:38 +0200

    Update test.cs

--- test.cs
+++ test.cs
@@ -9,5 +9,12 @@ namespace HalloWorld
             return(v1*v2);
         }
     }
-
+class CalculateClass
+    {
+        
+        public int ProductOf2(int v1, int v2)
+        {
+            return(v1*v2);
+        }
+    }
 }
