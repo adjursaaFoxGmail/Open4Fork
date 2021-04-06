98e238537d11a2e9454a370cce38ee2a7b599e5b
adjursaaFoxGmail <72859988+adjursaaFoxGmail@users.noreply.github.com>
2021-04-06 16:51:40 +0200

    Create test.cs

---
+++ test.cs
@@ -0,0 +1,13 @@
+using System;
+namespace HalloWorld
+{
+    class CalculateClass
+    {
+        
+        public int ProductOf2(int v1, int v2)
+        {
+            return(v1*v2);
+        }
+    }
+
+}
